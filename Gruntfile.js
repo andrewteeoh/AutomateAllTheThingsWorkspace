7818cd2296afec552ee8a084058e25bc975eda80
Andrew Tio <andrewmtio@gmail.com>
2014-05-23 10:25:03 -0700

    Add the version task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,23 @@ module.exports = function(grunt) {
             files: [ { src: './**', dest: './dist/' } ]
         }
     },
+    version: {
+      options: {
+          buildNumber: "1.0." + (+new Date())
+      }
+    },
     clean: ['./dist'],
   });
 
   // Instead of grunt.loadNpmTasks('...')
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('default', ['copy', 'clean']);
+  grunt.registerTask('version', 'Add build number to the package.json', function() {
+    var package = JSON.parse(grunt.file.read('./package.json'));
+    package.version = this.options().buildNumber;
+
+    grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
+  });
+
+  grunt.registerTask('default', ['clean', 'copy', 'version']);
 };
