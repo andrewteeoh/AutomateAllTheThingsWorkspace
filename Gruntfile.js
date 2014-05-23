a95e972114fd0f4390bbefe873c9d9e8ad3165a0
Andrew Tio <andrewmtio@gmail.com>
2014-05-23 10:46:29 -0700

    Create multi target task "version"
    Add dev target

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,22 +6,35 @@ module.exports = function(grunt) {
         }
     },
     version: {
-      options: {
+      release: {
+        options: {
           buildNumber: "1.0." + (+new Date())
+        }
+      },
+      dev: {
+        options: {
+            buildNumber: function() {
+                var package = JSON.parse(grunt.file.read('./package.json'));
+                var chunks = package.version.split('.');
+                chunks[1] = parseInt(chunks[1], 10) + 1;
+                return chunks.join('.') + '-pre';
+            }()
+        }
       }
     },
     clean: ['./dist'],
   });
 
   // Instead of grunt.loadNpmTasks('...')
+  // https://github.com/sindresorhus/load-grunt-tasks
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('version', 'Add build number to the package.json', function() {
+  grunt.registerMultiTask('version', 'Add build number to the package.json', function() {
     var package = JSON.parse(grunt.file.read('./package.json'));
     package.version = this.options().buildNumber;
 
     grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
   });
 
-  grunt.registerTask('default', ['clean', 'copy', 'version']);
+  grunt.registerTask('default', ['clean', 'copy', 'version:release']);
 };
